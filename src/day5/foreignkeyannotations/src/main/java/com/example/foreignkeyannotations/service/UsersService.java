506382591a3762cfc05b5b73c1181fe2b36356fc
Akash Tiwari <akash.tiwari@nucleusteq.com>
2025-05-14 15:48:01 +0530

    day 5

---
+++ src/day5/foreignkeyannotations/src/main/java/com/example/foreignkeyannotations/service/UsersService.java
@@ -0,0 +1,13 @@
+package com.example.foreignkeyannotations.service;
+
+import com.example.foreignkeyannotations.dto.UserDTO;
+import com.example.foreignkeyannotations.entity.Address;
+import com.example.foreignkeyannotations.entity.Users;
+
+import java.util.List;
+
+public interface UsersService {
+
+    UserDTO createUser(UserDTO userDTO);
+    List<UserDTO> getAllUsers();
+}
